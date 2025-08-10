dd4fbada97241ee276470ea23ae48ebfd5b78fc8
nichoalx <nicholasalexanderjosia@gmail.com>
2025-08-10 22:45:51 +0800

    hmm

--- rockquest/app/(tabs)/_layout.tsx
+++ rockquest/app/(tabs)/_layout.tsx
@@ -2,18 +2,17 @@ import { Tabs } from "expo-router";
 
 export default function TabLayout() {
   return (
-    <Tabs screenOptions={{ headerShown: false }}>
+    <Tabs screenOptions={{ headerShown: false, tabBarStyle: { display: "none" } }}>
       {/* Top-level auth/onboarding screens (still in (tabs)/) */}
-      <Tabs.Screen name="index" options={{ title: "Home", tabBarStyle: { display: "none" } }} />
+      <Tabs.Screen name="auth" options={{ title: "Auth", tabBarStyle: { display: "none" } }} />
+      <Tabs.Screen name="index" options={{ title: "Home", tabBarStyle: { display: "none" }}} />
       <Tabs.Screen name="welcomeScreen" options={{ title: "Welcome to RockQuest", tabBarStyle: { display: "none" } }} />
       <Tabs.Screen name="choose-role" options={{ title: "Role", tabBarStyle: { display: "none" } }} />
-      <Tabs.Screen name="profile-info" options={{ title: "Profile Info", tabBarStyle: { display: "none" } }} />
       <Tabs.Screen name="signup-details" options={{ title: "Signup details", tabBarStyle: { display: "none" } }} />
       <Tabs.Screen name="upload-documents" options={{ title: "Upload documents", tabBarStyle: { display: "none" } }} />
       <Tabs.Screen name="thankyouScreen" options={{ title: "Finish", tabBarStyle: { display: "none" } }} />
 
       {/* Players */}
-      <Tabs.Screen name="players/auth" options={{ title: "Auth", tabBarStyle: { display: "none" } }} />
       <Tabs.Screen name="players/dashboard" options={{ title: "Dashboard", tabBarStyle: { display: "none" } }} />
       <Tabs.Screen name="players/collections" options={{ title: "Collections", tabBarStyle: { display: "none" } }} />
       <Tabs.Screen name="players/collection-rock" options={{ title: "Rock collection", tabBarStyle: { display: "none" } }} />
